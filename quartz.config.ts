cfd7ba7be10ed221c8ed25ac7b2ca62a6990646b
tye021122 <79403226+tye021122@users.noreply.github.com>
2026-09-05 00:10:25 +0900

    마지막

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ko-KR",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "noteoftye.pages.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
